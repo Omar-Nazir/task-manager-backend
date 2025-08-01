a3da3168b2dc3257cda5cd5995a1a9977aa3ed60
Omar-Nazir <backupun2941@gmail.com>
2025-08-01 20:25:41 +0500

    domain issue resolved

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.REACT_APP_Port;
+const PORT =  "https://task-manager-backend-production-dcec.up.railway.app/" || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://umar:helloword@cluster0.nlbsq6s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect("mongodb+srv://umar:helloword@cluster0.nlbsq6s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'));

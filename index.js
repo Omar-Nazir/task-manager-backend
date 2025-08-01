654f53f214f1ad0ca07207224129fa8cb2a043eb
Omar-Nazir <backupun2941@gmail.com>
2025-08-01 18:14:01 +0500

    path issue resolved

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.REACT_APP_Port ||5000;
+const PORT = process.env.REACT_APP_Port;
 
 app.use(cors());
 app.use(express.json());

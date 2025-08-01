80b526aa8219155f24568d9fd289e0d8de020a20
Omar-Nazir <backupun2941@gmail.com>
2025-08-01 10:39:14 +0500

    atlse connected

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.REACT_APP_Port ||5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/taskdb', {
+mongoose.connect('mongodb+srv://umar:helloword@cluster0.nlbsq6s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'));

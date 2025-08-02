b740632ca6b5cd010e42eb861af3554a084d7af0
Umar Nazir <backupun2941@gmail.com>
2025-08-02 11:00:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT =  "https://task-manager-backend-production-dcec.up.railway.app/" || 5000;
+const PORT =  process.env.REACT_APP_URL || 5000 ;
 
 app.use(cors());
 app.use(express.json());
